e20e6d0a6cb3b83b78ff4b4c6588ae0c7e459710
deedub <dwormald12@gmail.com>
2017-06-19 20:10:52 +1000

    splash & menu

---
+++ OverTheWall/Assets/Scripts/Shared/Fading.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fading : MonoBehaviour {
+
+    public Texture2D fadeOutTexture;
+    public float fadeSpeed = 1.0f;
+
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+    private int fadeDir = -1;
+
+    private void OnGUI()
+    {
+        //fade out/in the alpha value using a direction, a speed and time.deltatime to convert the operation into seconds
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+
+        //force (clamp) the number between  0 and 1
+        alpha = Mathf.Clamp01(alpha);
+
+        //Set color of out GUI
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+
+        //make sure black texture will render on top
+        GUI.depth = drawDepth;
+
+        //draw texture to fit on screen
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+    }
+
+    public float BeginFade(int direction)
+    {
+        fadeDir = direction;
+
+        return fadeSpeed;
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        BeginFade(-1);
+    }
+}
---
+++ OverTheWall/Assets/Splash_Debug.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash_Debug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpashTime());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator SpashTime()
+    {
+        yield return new WaitForSeconds(3);
+        
+        SceneManager.LoadScene("MenuScene");
+    }
+}
